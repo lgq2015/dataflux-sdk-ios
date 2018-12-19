70ca959afefb2efd78804301a49e1b98aac0cc19
hulilei <huuuu1016@gmail.com>
2018-12-19 16:15:20 +0800

    照片选择类添加

---
+++ App/Utils/PWPhotoOrAlbumImagePicker/PWPhotoOrAlbumImagePicker.h
@@ -0,0 +1,22 @@
+//
+//  PWPhotoOrAlbumImagePicker.h
+//  App
+//
+//  Created by 胡蕾蕾 on 2018/12/19.
+//  Copyright © 2018 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+typedef void (^PWPhotoOrAlbumImagePickerBlock)(UIImage *image);
+
+@interface PWPhotoOrAlbumImagePicker : NSObject
+/**
+ 公共方法 选择图片后的图片回掉
+ 
+ @param controller 使用这个工具的控制器
+ @param photoBlock 选择图片后的回掉
+ */
+- (void)getPhotoAlbumOrTakeAPhotoWithController:(UIViewController *)controller photoBlock:(PWPhotoOrAlbumImagePickerBlock)photoBlock;
+
+
+@end
