e1638370a697c565ecfe81f507e148632ea122fb
hulilei <huuuu1016@gmail.com>
2019-01-07 18:09:56 +0800

    常用组件封装

---
+++ App/Utils/Other/PWCommonCtrl.h
@@ -0,0 +1,17 @@
+//
+//  PWCommonCtrl.h
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/1/7.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PWCommonCtrl : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ App/Utils/Other/PWCommonCtrl.m
@@ -0,0 +1,13 @@
+//
+//  PWCommonCtrl.m
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/1/7.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "PWCommonCtrl.h"
+
+@implementation PWCommonCtrl
+
+@end
