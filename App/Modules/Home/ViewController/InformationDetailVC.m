852fb5a988da7aa47a4d3dc6ae796dc6c40a4b3b
hulilei <huuuu1016@gmail.com>
2019-01-16 11:07:53 +0800

    BadgeButton add

---
+++ App/Modules/Home/ViewController/InformationDetailVC.m
@@ -0,0 +1,36 @@
+//
+//  InformationDetailVC.m
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/1/16.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "InformationDetailVC.h"
+
+@interface InformationDetailVC ()
+
+@end
+
+@implementation InformationDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"情报详情";
+    [self createUI];
+    // Do any additional setup after loading the view.
+}
+- (void)createUI{
+    
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ App/Utils/BadgeButton/BadgeButton.h
@@ -0,0 +1,19 @@
+//
+//  BadgeButton.h
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/1/16.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface BadgeButton : UIButton
+@property(nonatomic) NSInteger badgeValue;
+
+@property(nonatomic, assign) BOOL isRedBall;
+@end
+
+
