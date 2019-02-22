d8f64197b698044959c8e64faae8e84d5def085e
hulilei <huuuu1016@gmail.com>
2019-02-22 18:32:38 +0800

    简易webview添加

---
+++ App/Modules/Home/ViewController/NewsWebView.h
@@ -0,0 +1,17 @@
+//
+//  NewsWebView.h
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/2/22.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "PWBaseWebVC.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewsWebView : PWBaseWebVC
+
+@end
+
+NS_ASSUME_NONNULL_END
