74ce08450cfce08b2465c41581437330881fd02b
hulilei <huuuu1016@gmail.com>
2019-02-25 19:02:47 +0800

    icon添加

---
+++ App/Modules/Home/View/RightTriangleView.h
@@ -0,0 +1,17 @@
+//
+//  RightTriangleView.h
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/2/25.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RightTriangleView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
