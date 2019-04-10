2027427cae67aea9b842be75a00a60b72183162d
tao <315226680@qq.com>
2019-04-10 21:12:01 +0800

    文件预览txt zip兼容处理

---
+++ App/Utils/Other/QLPreviewController+title.h
@@ -0,0 +1,17 @@
+//
+//  QLPreviewController+title.h
+//  App
+//
+//  Created by tao on 2019/4/10.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import <QuickLook/QuickLook.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QLPreviewController (title)
+@property (nonatomic, copy)NSString *qltitle;
+@end
+
+NS_ASSUME_NONNULL_END
