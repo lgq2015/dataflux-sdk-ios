a91dc9c92976213f4a5b6326bd1cc752540bcd84
tao <315226680@qq.com>
2019-04-17 20:26:48 +0800

    创建团队，选择所在地（已选城市），二次进入显示修复

--- App/Modules/Team/ViewController/ChooseAddressVC.m
+++ App/Modules/Team/ViewController/ChooseAddressVC.m
@@ -15,6 +15,8 @@
 @property (nonatomic, strong) UILabel *currentDisLab;
 @property (nonatomic, strong) NSMutableArray *cityAry;
 @property (nonatomic, strong) UICollectionView *cityCollectionView;
+@property (nonatomic, assign) NSInteger selectCityInd; //已选城市的索引
+@property (nonatomic, assign) NSInteger selectProvInd; //已选省份的索引
 @property (nonatomic, copy) NSString *province;
 @end
 @implementation ChooseAddressVC
@@ -36,8 +38,12 @@
             NSArray *district = content[@"district"];
             if (district.count>0) {
                 self.districtAry = [NSMutableArray arrayWithArray:district];
+                //再次进入，定位所选城市
+                _selectProvInd = [self selectedProvinceIndex];
+                [self setFistSelect:_selectProvInd];
+                _selectCityInd = [self selectedCityIndex];
                 [self.tableView reloadData];
-                [self setFistSelect];
+                [self.cityCollectionView reloadData];
             }
             [SVProgressHUD dismiss];
             
@@ -47,24 +53,14 @@
     }];
     
 }
-- (void)setFistSelect{
-//    [self.districtAry enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        
-//    }];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    DistrictCell *cell = (DistrictCell *)[self.tableView cellForRowAtIndexPath:indexPath];
-    [cell setSelected:YES];
+- (void)setFistSelect:(NSInteger)selectedIndex{
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:selectedIndex inSection:0];
     self.province = self.districtAry[indexPath.row][@"name"];
-    [self.cityCollectionView reloadData];
-
     NSDictionary *dict = self.districtAry[indexPath.row];
     if (self.cityAry.count) {
         [self.cityAry removeAllObjects];
-        [self.cityCollectionView reloadData];
     }
     [self.cityAry addObjectsFromArray:dict[@"children"]];
-    [self.cityCollectionView reloadData];
-
 }
 - (void)createUI{
     self.tableView.rowHeight =ZOOM_SCALE(50);
@@ -124,7 +120,6 @@
         [layout setScrollDirection:UICollectionViewScrollDirectionVertical];
         layout.sectionInset = UIEdgeInsetsMake(10,10,20,10);
         //该方法也可以设置itemSize
-//        layout.itemSize =CGSizeMake(kWidth-2*Interval(16), ZOOM_SCALE(64));
         layout.itemSize = CGSizeMake(ZOOM_SCALE(125), ZOOM_SCALE(40));
 
         layout.minimumLineSpacing = 10;
@@ -147,20 +142,15 @@
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    DistrictCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DistrictCell"];
+    DistrictCell *cell = (DistrictCell *)[tableView dequeueReusableCellWithIdentifier:@"DistrictCell"];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     NSString *name =self.districtAry[indexPath.row][@"name"];
     cell.title = name;
-    
-    self.province = self.districtAry[indexPath.row][@"name"];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
     return cell;
 }
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (indexPath.row!=0) {
-        NSIndexPath *indexPath1 = [NSIndexPath indexPathForRow:0 inSection:0];
-        DistrictCell *cell = (DistrictCell *)[self.tableView cellForRowAtIndexPath:indexPath1];
-        [cell setSelected:NO];
-    }
+    _selectProvInd = indexPath.row;
     NSDictionary *dict = self.districtAry[indexPath.row];
     if (self.cityAry.count) {
         [self.cityAry removeAllObjects];
@@ -169,39 +159,72 @@
     [self.cityAry addObjectsFromArray:dict[@"children"]];
     self.province = self.districtAry[indexPath.row][@"name"];
     [self.cityCollectionView reloadData];
+    [self.tableView reloadData];
+}
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (tableView == self.tableView){
+        if(indexPath.row == _selectProvInd){
+            cell.selected = YES;
+        }else{
+            cell.selected = NO;
+        }
+    }
+    
 }
 #pragma mark ========== ui ==========
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     return self.cityAry.count;
 }
 
-
 - (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
     CityCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CityCell" forIndexPath:indexPath];
     NSString *name =self.cityAry[indexPath.row][@"name"];
     cell.titleLab.text = name;
-    
     return cell;
 }
+- (void)collectionView:(UICollectionView *)collectionView willDisplayCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.row == _selectCityInd && [self.province isEqualToString:self.currentProvince]){
+        cell.selected = YES;
+    }else{
+        cell.selected = NO;
+    }
+}
 -(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
-   
      CityCell *cell = (CityCell *)[collectionView cellForItemAtIndexPath:indexPath];
      NSString *text = [NSString stringWithFormat:@"%@ %@",self.province,cell.titleLab.text];
-    
     if (self.itemClick) {
         self.itemClick(text);
         [self.navigationController popViewControllerAnimated:YES];
     }
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//获取已选省份的索引
+- (NSInteger)selectedProvinceIndex{
+    __block NSInteger provinceIndex = 0;
+    if (self.currentProvince.length > 0){
+        [self.districtAry enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSDictionary *dic = (NSDictionary *)obj;
+            NSString *name = [dic stringValueForKey:@"name" default:@""];
+            if ([name isEqualToString:self.currentProvince]){
+                provinceIndex = idx;
+                *stop = NO;
+            }
+        }];
+    }
+    return provinceIndex;
 }
-*/
-
+//获取已选城市的索引
+- (NSInteger)selectedCityIndex{
+    __block NSInteger cityIndex = 0;
+    if (self.currentCity.length > 0){
+        [self.cityAry enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            NSDictionary *dic = (NSDictionary *)obj;
+            NSString *name = [dic stringValueForKey:@"name" default:@""];
+            if ([name isEqualToString:self.currentCity]){
+                cityIndex = idx;
+                *stop = NO;
+            }
+        }];
+    }
+    return cityIndex;
+}
 @end
