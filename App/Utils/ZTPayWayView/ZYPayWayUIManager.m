edbe03bbaf9d7910e5b5d5e241c234425dc8f32e
tao <315226680@qq.com>
2019-04-22 21:24:08 +0800

    支付方式 选择UI 部分功能实现

--- App/Utils/ZTPayWayView/ZYPayWayUIManager.m
+++ App/Utils/ZTPayWayView/ZYPayWayUIManager.m
@@ -5,10 +5,8 @@
 //  Created by tao on 2019/4/22.
 //  Copyright © 2019 hll. All rights reserved.
 //
-#define zt_platform_itemH 93
-#define zt_platform_cancelBtnH 60
-#define zt_cancel_topline_margin 15 //处理分割线距离上方的大小
-#define zt_payWayViewH zt_platform_itemH * 2
+#define PayWayViewH 280.0
+
 #import "ZYPayWayUIManager.h"
 @interface ZYPayWayUIManager()
 @property (nonatomic,strong) UIWindow * window;
@@ -39,18 +37,24 @@
     [self.window addSubview:self.backgroundGrayView];
     [self.window addSubview:self.payWayView];
     [self.payWayView addSubview:self.payWayLab];
+    [self.payWayView addSubview:self.zhifubaoLab];
+    [self.payWayView addSubview:self.contactsaleLab];
+    [self.payWayView addSubview:self.zhifubaoBtn];
+    [self.payWayView addSubview:self.contactsaleBtn];
+    [self.payWayView addSubview:self.surePayBtn];
+    [self.payWayView addSubview:self.closeBtn];
     [self p_hideFrame];
     [self s_childUI];
 }
 
 //隐藏
 -(void)p_hideFrame{
-    _payWayView.frame =CGRectMake(0,kHeight, kWidth, 300);
+    _payWayView.frame =CGRectMake(0,kHeight, kWidth, PayWayViewH);
 }
 
 //展示
 -(void)p_disFrame{
-    _payWayView.frame =CGRectMake(0,kHeight-300, kWidth, 300);
+    _payWayView.frame =CGRectMake(0,kHeight-PayWayViewH, kWidth, PayWayViewH);
 }
 #pragma mark - 布局子控件
 - (void)s_childUI{
@@ -61,6 +65,37 @@
     [self.zhifubaoLab mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self.payWayLab.mas_left);
         make.top.equalTo(self.payWayLab.mas_bottom).offset(27);
+    }];
+    [self.contactsaleLab mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.payWayLab.mas_left);
+        make.top.equalTo(self.zhifubaoLab.mas_bottom).offset(27);
+    }];
+    [self.zhifubaoBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.zhifubaoLab);
+        //TODO:zhangtao
+//        make.right.equalTo(self.payWayView);
+//        make.width.equalTo(@76);
+//        make.height.equalTo(@36);
+        make.right.equalTo(self.payWayView).offset(-28);
+        make.width.equalTo(@20);
+        make.height.equalTo(@20);
+    }];
+    [self.contactsaleBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(self.contactsaleLab);
+        make.width.equalTo(self.zhifubaoBtn.mas_width);
+        make.height.equalTo(self.zhifubaoBtn.mas_height);
+        make.right.equalTo(self.zhifubaoBtn.mas_right);
+    }];
+    [self.surePayBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.payWayView.mas_bottom).offset(-21);
+        make.left.equalTo(self.payWayView.mas_left).offset(16);
+        make.right.equalTo(self.payWayView.mas_right).offset(-16);
+        make.height.equalTo(@47);
+    }];
+    [self.closeBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.payWayView.mas_top).offset(8);
+        make.right.equalTo(self.payWayView.mas_right).offset(-15);
+        make.width.height.equalTo(@47);
     }];
 }
 #pragma mark - 展示
@@ -156,36 +191,59 @@
 - (UILabel *)zhifubaoLab{
     if (!_zhifubaoLab){
         _zhifubaoLab = [[UILabel alloc] init];
+        _zhifubaoLab.font = [UIFont systemFontOfSize:20];
+        _zhifubaoLab.text = @"支付宝";
     }
     return _zhifubaoLab;
 }
 - (UILabel *)contactsaleLab{
     if (!_contactsaleLab){
         _contactsaleLab = [[UILabel alloc] init];
+        _contactsaleLab.font = [UIFont systemFontOfSize:20];
+        _contactsaleLab.text = @"联系销售";
     }
     return _contactsaleLab;
 }
 - (UIButton *)zhifubaoBtn{
     if (!_zhifubaoBtn){
         _zhifubaoBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_zhifubaoBtn setBackgroundImage:[UIImage imageWithColor:[UIColor grayColor]] forState:UIControlStateNormal];
+        [_zhifubaoBtn setBackgroundImage:[UIImage imageWithColor:[UIColor blueColor]] forState:UIControlStateSelected];
+        //TODO:zhangtao
+        _zhifubaoBtn.layer.cornerRadius = 10;
+        _zhifubaoBtn.layer.masksToBounds = YES;
+        _zhifubaoBtn.selected = YES;
     }
     return _zhifubaoBtn;
 }
 - (UIButton *)contactsaleBtn{
     if (!_contactsaleBtn){
         _contactsaleBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_contactsaleBtn setBackgroundImage:[UIImage imageWithColor:[UIColor grayColor]] forState:UIControlStateNormal];
+        [_contactsaleBtn setImage:[UIImage imageWithColor:[UIColor blueColor]] forState:UIControlStateSelected];
+        //TODO:zhangtao
+        _contactsaleBtn.layer.cornerRadius = 10;
+        _contactsaleBtn.layer.masksToBounds = YES;
     }
     return _contactsaleBtn;
 }
 - (UIButton *)surePayBtn{
     if (!_surePayBtn){
         _surePayBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _surePayBtn.backgroundColor = [UIColor colorWithHexString:@"#2A7AF7"];
+        _surePayBtn.layer.cornerRadius = 23.5;
+        _surePayBtn.layer.masksToBounds = YES;
+        _surePayBtn.titleLabel.font = [UIFont systemFontOfSize:20];
+        [_surePayBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_surePayBtn setTitle:@"￥1000，000  去支付" forState:UIControlStateNormal];
     }
     return _surePayBtn;
 }
 - (UIButton *)closeBtn{
     if (!_closeBtn){
         _closeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_closeBtn setImage:[UIImage imageNamed:@"icon_payway_close"] forState:UIControlStateNormal];
+        [_closeBtn addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
     }
     return _closeBtn;
 }
