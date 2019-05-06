3fee685afa296c76d9fc608e5fd9c2c27f8596e1
hulilei <huuuu1016@gmail.com>
2019-05-06 21:46:59 +0800

    过去24小时列表时间显示更改

--- App/Modules/Home/Model/IssueListViewModel.m
+++ App/Modules/Home/Model/IssueListViewModel.m
@@ -9,6 +9,8 @@
 #import "IssueListViewModel.h"
 #import "IssueModel.h"
 #import "IssueSourceManger.h"
+#import "IssueChatDataManager.h"
+
 @implementation IssueListViewModel
 - (instancetype)initWithJsonDictionary:(IssueModel *)model{
     if (![model isKindOfClass:[IssueModel class]]) return nil;
@@ -28,7 +30,7 @@
 
     [model checkInvalidIssue];
     self.isInvalidIssue = model.isInvalidIssue;
-
+    self.time = [NSString getLocalDateFormateUTCDate:model.createTime formatter:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
     if ([model.level isEqualToString:@"danger"]) {
         self.state = MonitorListStateSeriousness;
     }else if([model.level isEqualToString:@"warning"]){
@@ -38,12 +40,11 @@
     }
     if ([model.status isEqualToString:@"recovered"]) {
         self.state =MonitorListStateRecommend;
+        self.time = [NSString getLocalDateFormateUTCDate:model.endTime formatter:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
     }else if ([model.status isEqualToString:@"discarded"]){
         self.state = MonitorListStateLoseeEfficacy;
     }
-    NSArray *latestIssueLogs = [model.latestIssueLogsStr jsonValueDecoded];
     
-    NSDictionary *issueLogDict =latestIssueLogs[0];
     if (![model.renderedTextStr isEqualToString:@""]) {
         NSDictionary *dict = [model.renderedTextStr jsonValueDecoded];
         self.title = [dict stringValueForKey:@"title" default:@""];
@@ -54,31 +55,8 @@
         self.title = model.title;
         self.content = model.content;
     }
-    NSDictionary *account_info = issueLogDict[@"account_info"];
-//    if (account_info.allKeys>0) {
-//        NSString *account = [account_info stringValueForKey:@"name" default:@""];
-//        NSString *nickname = [account_info stringValueForKey:@"nickname" default:@""];
-//        NSString *type = [issueLogDict stringValueForKey:@"type" default:@""];
-//        NSString *content;
-//        if ([type isEqualToString:@"attachment"]) {
-//            NSString *fileName = [issueLogDict[@"metaJSON"] stringValueForKey:@"originalFileName" default:@""];
-//             NSString *type =  [fileName pathExtension];
-//            if([type isEqualToString:@"jpg"]||[type isEqualToString:@"png"]||[type isEqualToString:@"jpeg"]){
-//                content =@"[图片]";
-//            }else{
-//                content = [NSString stringWithFormat:@"[文件]%@",fileName];
-//            }
-//        }else{
-//            content =[issueLogDict stringValueForKey:@"content" default:@""];
-//        }
-//        if (nickname.length>0) {
-//            self.issueLog =[NSString stringWithFormat:@"%@:  %@",nickname,content];
-//        }else{
-//            self.issueLog =[NSString stringWithFormat:@"%@: %@",account,content];
-//        }
-//    }else{
-//        self.issueLog = [issueLogDict stringValueForKey:@"content" default:@""];
-//    }
+  
+
     NSDictionary *markTookOverInfoJSON,*markEndAccountInfo;
     if (model.markTookOverInfoJSONStr) {
         markTookOverInfoJSON = [model.markTookOverInfoJSONStr jsonValueDecoded];
@@ -91,18 +69,17 @@
     }
     if ([model.markStatus isEqualToString:@"tookOver"]){
        
-        NSString *name = [[markTookOverInfoJSON stringValueForKey:@"nickname" default:@""] isEqualToString:@""]?[markTookOverInfoJSON stringValueForKey:@"name" default:@""]:[markTookOverInfoJSON stringValueForKey:@"nickname" default:@""];
+        NSString *name = [markTookOverInfoJSON stringValueForKey:@"name" default:@""];
         self.issueLog = [NSString stringWithFormat:@"· %@正在处理",name];
         
     }else if ([model.markStatus isEqualToString:@"recovered"]){
-        //[4]    (null)    @"originInfoAccountId_cache" : @"acnt-9RS9ntgyqPvFLSs61kqggs"
-       
-        NSString *name = [[markEndAccountInfo stringValueForKey:@"nickname" default:@""] isEqualToString:@""]?[markEndAccountInfo stringValueForKey:@"name" default:@""]:[markEndAccountInfo stringValueForKey:@"nickname" default:@""];
+
+        NSString *name = [markEndAccountInfo stringValueForKey:@"name" default:@""];
         self.issueLog = [NSString stringWithFormat:@"· 已由%@解决",name];
     
         
     }
-    self.time = [NSString getLocalDateFormateUTCDate:model.createTime formatter:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
+   
     if ([model.origin isEqualToString:@"user"]) {
         self.isFromUser = YES;
     }else{
@@ -110,6 +87,15 @@
     }
     if (model.latestIssueLogsStr) {
         self.isHasChat = YES;
+    }
+    if (model.readAtInfoStr) {
+        NSDictionary *readAtInfo = [model.readAtInfoStr jsonValueDecoded];
+        int unreadCount = [readAtInfo intValueForKey:@"unreadCount" default:0];
+        long long lastReadSeq = [readAtInfo longLongValueForKey:@"lastReadSeq" default:0];
+        long long seq = [[IssueChatDataManager sharedInstance] getLastChatIssueLogMarker:model.issueId];
+        if (unreadCount>0 && lastReadSeq<seq) {
+         self.isCallME = YES;
+        }
     }
     self.ticketStatus = model.ticketStatus;
     self.isRead = model.isRead;
