ebe67cc42a97f2f8cb2b5fdc1edde61562921700
hulilei <huuuu1016@gmail.com>
2019-05-07 21:33:00 +0800

    teamcell 多选UI

--- App/Modules/Team/View/TeamMemberCell.m
+++ App/Modules/Team/View/TeamMemberCell.m
@@ -9,8 +9,8 @@
 #import "TeamMemberCell.h"
 #import "MemberInfoModel.h"
 @interface TeamMemberCell()
-@property (nonatomic, strong) UIImageView *iconImgView;
-@property (nonatomic, strong) UILabel *titleLab;
+//@property (nonatomic, strong) UIImageView *iconImgView;
+//@property (nonatomic, strong) UILabel *titleLab;
 @property (nonatomic, strong) UILabel *adminLab;
 @property (nonatomic, strong) UILabel *beizhuLab;
 @end
@@ -22,48 +22,78 @@
 }
 -(void)setModel:(MemberInfoModel *)model{
     _model = model;
-    self.titleLab.text = _model.name;
     self.beizhuLab.text = _model.inTeamNote;
     NSString *img = [_model.tags stringValueForKey:@"pwAvatar" default:@""];
-    if (model.isSpecialist){
-        [self.iconImgView setImage:[UIImage imageNamed:@"professor_wang_header"]];
-    }else{
-        [self.iconImgView sd_setImageWithURL:[NSURL URLWithString:img] placeholderImage:[UIImage imageNamed:@"team_memicon"]];
-    }
-}
--(void)layoutSubviews{
-    [self.iconImgView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(self.contentView).offset(Interval(16));
+    UIImageView *iconImgView = [[UIImageView alloc]init];
+    iconImgView.layer.cornerRadius = ZOOM_SCALE(20);
+    iconImgView.layer.masksToBounds = YES;
+    iconImgView.tag = 33;
+    [[self.contentView viewWithTag:33] removeFromSuperview];
+    [self.contentView addSubview:iconImgView];
+    [iconImgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        if (model.isMultiChoice) {
+            make.left.mas_equalTo(self.contentView).offset(Interval(47));
+        }else{
+            make.left.mas_equalTo(self.contentView).offset(Interval(16));
+        }
         make.width.height.mas_equalTo(ZOOM_SCALE(40));
         make.centerY.mas_equalTo(self.contentView);
     }];
+    [self.contentView sendSubviewToBack:iconImgView];
     
-    [self.titleLab mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(self.iconImgView.mas_right).offset(Interval(13));
+
+    UILabel *titleLab = [PWCommonCtrl lableWithFrame:CGRectZero font:RegularFONT(16) textColor:[UIColor colorWithHexString:@"#373D41"] text:@""];
+    titleLab.tag = 34;
+    [[self.contentView viewWithTag:34] removeFromSuperview];
+    [self.contentView addSubview:titleLab];
+    titleLab.text = _model.name;
+    [titleLab mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(iconImgView.mas_right).offset(Interval(13));
         make.height.offset(ZOOM_SCALE(22));
-        make.centerY.mas_equalTo(self.iconImgView);
+        make.centerY.mas_equalTo(iconImgView);
     }];
     [self.beizhuLab mas_makeConstraints:^(MASConstraintMaker *make) {
         make.right.mas_equalTo(self.contentView).offset(-Interval(22));
-        make.left.mas_greaterThanOrEqualTo(self.titleLab.mas_right).offset(10);
+        make.left.mas_greaterThanOrEqualTo(titleLab.mas_right).offset(10);
         make.bottom.equalTo(self.contentView.mas_bottom).offset(-13);
     }];
     [self.line mas_makeConstraints:^(MASConstraintMaker *make) {
         make.height.offset(1);
-        make.left.mas_equalTo(self.titleLab);
+        make.left.mas_equalTo(self.contentView).offset(ZOOM_SCALE(40)+Interval(16));
         make.right.mas_equalTo(self.contentView);
         make.bottom.mas_equalTo(self.contentView);
     }];
     [self.adminLab mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(self.iconImgView).offset(ZOOM_SCALE(31));
-        make.centerX.mas_equalTo(self.iconImgView);
+        make.top.mas_equalTo(iconImgView).offset(ZOOM_SCALE(31));
+        make.centerX.mas_equalTo(iconImgView);
         make.width.offset(ZOOM_SCALE(40));
         make.height.offset(ZOOM_SCALE(14));
     }];
-    
+    [iconImgView bringSubviewToFront:self.adminLab];
     self.adminLab.hidden = _model.isAdmin || _model.isSpecialist?NO:YES;
-    [self.titleLab setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
+    [titleLab setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
     [self.beizhuLab setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+    if (model.isSpecialist){
+        [iconImgView setImage:[UIImage imageNamed:@"professor_wang_header"]];
+    }else{
+        [iconImgView sd_setImageWithURL:[NSURL URLWithString:img] placeholderImage:[UIImage imageNamed:@"team_memicon"]];
+    }
+    if (model.isMultiChoice) {
+        UIButton *selImage = [[UIButton alloc]init];
+        [selImage setImage:[UIImage imageNamed:@"icon_noSelect"] forState:UIControlStateNormal];
+        [selImage setImage:[UIImage imageNamed:@"team_success"] forState:UIControlStateSelected];
+        selImage.tag = 55;
+        [[self.contentView viewWithTag:55] removeFromSuperview];
+        [self.contentView addSubview:selImage];
+        [selImage mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.mas_equalTo(self.contentView).offset(Interval(16));
+            make.width.height.offset(20);
+            make.centerY.mas_equalTo(self.contentView);
+        }];
+    }
+}
+-(void)layoutSubviews{
+   
 }
 -(UILabel *)adminLab{
     if (!_adminLab) {
@@ -89,22 +119,22 @@
     }
     return _line;
 }
-- (UIImageView *)iconImgView{
-    if (!_iconImgView) {
-        _iconImgView = [[UIImageView alloc]init];
-        _iconImgView.layer.cornerRadius = ZOOM_SCALE(20);
-        _iconImgView.layer.masksToBounds = YES;
-        [self.contentView addSubview:_iconImgView];
-    }
-    return _iconImgView;
-}
--(UILabel *)titleLab{
-    if (!_titleLab) {
-        _titleLab = [PWCommonCtrl lableWithFrame:CGRectZero font:RegularFONT(16) textColor:[UIColor colorWithHexString:@"#373D41"] text:@""];
-        [self.contentView addSubview:_titleLab];
-    }
-    return _titleLab;
-}
+//- (UIImageView *)iconImgView{
+//    if (!_iconImgView) {
+//        _iconImgView = [[UIImageView alloc]init];
+//        _iconImgView.layer.cornerRadius = ZOOM_SCALE(20);
+//        _iconImgView.layer.masksToBounds = YES;
+//        [self.contentView addSubview:_iconImgView];
+//    }
+//    return _iconImgView;
+//}
+//-(UILabel *)titleLab{
+//    if (!_titleLab) {
+//        _titleLab = [PWCommonCtrl lableWithFrame:CGRectZero font:RegularFONT(16) textColor:[UIColor colorWithHexString:@"#373D41"] text:@""];
+//        [self.contentView addSubview:_titleLab];
+//    }
+//    return _titleLab;
+//}
 
 - (UILabel *)beizhuLab{
     if (!_beizhuLab){
@@ -122,6 +152,10 @@
     UIWebView * callWebview = [[UIWebView alloc] init];
     [callWebview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:str]]];
     [self addSubview:callWebview];
+}
+- (void)setTeamMemberSelect{
+   UIButton *selimage= [self.contentView viewWithTag:55];
+    selimage.selected = !selimage.selected;
 }
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
