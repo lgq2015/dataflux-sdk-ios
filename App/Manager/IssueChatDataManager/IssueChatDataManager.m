ba45f6c640ffec9df05826c8757e42815508a540
章博 <heybozai@sina.com>
2019-05-09 20:21:35 +0800

    修改错误消息sql语句的书写问题

--- App/Manager/IssueChatDataManager/IssueChatDataManager.m
+++ App/Manager/IssueChatDataManager/IssueChatDataManager.m
@@ -59,7 +59,7 @@
         callback(model);
     }];
 
-    
+
 }
 
 /**
@@ -334,9 +334,7 @@
     [self.getHelper pw_inDatabase:^{
         NSString *table = PW_DB_ISSUE_ISSUE_LOG_TABLE_NAME;
         NSString *where = NSStringFormat(@"WHERE issueId='%@' OR (seq=0 AND origin='me')", issueId);
-        if(endSeq>0){
-            where= [where stringByAppendingFormat:@" AND seq > %lli  ", startSeq];
-        }
+        where= [where stringByAppendingFormat:@" AND seq > %lli  ", startSeq];
         if (startSeq > 0) {
             where= [where stringByAppendingFormat:@" AND seq < %lli  ", startSeq];
         }
