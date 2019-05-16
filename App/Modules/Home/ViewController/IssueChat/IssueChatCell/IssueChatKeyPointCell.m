c2f59c4432cc594c0183bfa6595d2eb94615b0a0
hulilei <huuuu1016@gmail.com>
2019-05-16 11:29:18 +0800

    icon 添加

---
+++ App/Modules/Home/ViewController/IssueChat/IssueChatCell/IssueChatKeyPointCell.m
@@ -0,0 +1,24 @@
+//
+//  IssueChatKeyPointCell.m
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/5/16.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "IssueChatKeyPointCell.h"
+
+@implementation IssueChatKeyPointCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
