c9d1947fa5cb89a20db669ecd53101f89ee4d57f
hulilei <huuuu1016@gmail.com>
2019-05-21 13:44:00 +0800

    icon替换

---
+++ App/Modules/Home/View/IgnoreItemView.m
@@ -0,0 +1,21 @@
+//
+//  IgnoreItemView.m
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/5/21.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "IgnoreItemView.h"
+
+@implementation IgnoreItemView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
