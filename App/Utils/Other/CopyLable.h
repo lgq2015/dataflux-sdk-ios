6ceacb51315563227d19800909a18c438526e235
hulilei <huuuu1016@gmail.com>
2019-07-23 15:28:51 +0800

    可全选复制的lable

---
+++ App/Utils/Other/CopyLable.h
@@ -0,0 +1,17 @@
+//
+//  CopyLable.h
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/7/23.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CopyLable : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ App/Utils/Other/CopyLable.m
@@ -0,0 +1,21 @@
+//
+//  CopyLable.m
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/7/23.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "CopyLable.h"
+
+@implementation CopyLable
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
