34e694e19458b18f0adbf7616f77d1cb0aa619fe
hulilei <huuuu1016@gmail.com>
2019-11-12 18:46:04 +0800

    统计视图model

--- App/Modules/Home/Model/ClassifyModel.h
+++ App/Modules/Home/Model/ClassifyModel.h
@@ -7,11 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
-
+typedef NS_ENUM(NSInteger ,ClassifyType){
+    ClassifyTypeCrontab = 1,//诊断
+    ClassifyTypeTask = 2,   //任务
+    ClassifyTypeAlarm = 3,  //告警
+    ClassifyTypeReport = 4, //报告
+};
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ClassifyModel : NSObject
-
+@property (nonatomic, strong) NSArray *dangerAry;
+@property (nonatomic, strong) NSArray *warningAry;
+@property (nonatomic, strong) NSArray *commonAry;
+@property (nonatomic, strong) NSArray *allAry;
+@property (nonatomic, assign) ClassifyType type;
+@property (nonatomic, strong) NSArray *echartDatas;
+@property (nonatomic, assign) CGFloat cellHeight;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *cellIdentifier;
 @end
 
 NS_ASSUME_NONNULL_END
