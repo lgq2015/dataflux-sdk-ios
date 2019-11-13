1df71d1f9a53f0e9e067b80804ebfe1c62dc790e
hulilei <huuuu1016@gmail.com>
2019-11-13 19:46:01 +0800

    add

---
+++ App/Modules/Home/ViewController/IssueReportListView.h
@@ -0,0 +1,17 @@
+//
+//  IssueReportListView.h
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/11/13.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IssueReportListView : RootViewController
+@property (nonatomic, strong) NSArray *datas;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ App/Modules/Home/ViewController/IssueReportListView.m
@@ -0,0 +1,76 @@
+//
+//  IssueReportListView.m
+//  App
+//
+//  Created by 胡蕾蕾 on 2019/11/13.
+//  Copyright © 2019 hll. All rights reserved.
+//
+
+#import "IssueReportListView.h"
+#import "MineCellModel.h"
+#import "MineViewCell.h"
+#import "IssueModel.h"
+#import "IssueListViewModel.h"
+#import "IssueDetailsVC.h"
+@interface IssueReportListView ()<UITableViewDelegate,UITableViewDataSource>
+
+@end
+
+@implementation IssueReportListView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self createUI];
+}
+- (void)createUI{
+    self.tableView.rowHeight = ZOOM_SCALE(45);
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.bounces = NO;
+    self.tableView.separatorInset = UIEdgeInsetsMake(0, 12, 0, 0);
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    self.tableView.showsVerticalScrollIndicator = NO;
+    if (@available(iOS 11.0, *)) {
+        self.tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
+    [self.tableView registerClass:[MineViewCell class] forCellReuseIdentifier:@"MineViewCell"];
+    self.tableView.frame = CGRectMake(0, 0, kWidth, kHeight-kTopHeight);
+    self.tableView.tableFooterView = self.footView;
+    [self.view addSubview:self.tableView];
+}
+#pragma mark ========== UITableViewDataSource ==========
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.datas.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+      MineViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MineViewCell"];
+    MineCellModel *model = [MineCellModel new];
+    IssueModel *issue = self.datas[indexPath.row];
+    
+    model.title =[NSString getLocalDateFormateUTCDate:issue.createTime formatter:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ" outdateFormatted:@"yyyy-MM-dd"];
+    [cell initWithData:model type:MineVCCellTypeOnlyTitle];
+    return cell;
+}
+#pragma mark ========== UITableViewDelegate ==========
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    IssueModel *model =self.datas[indexPath.row];
+    IssueListViewModel *viewModel = [[IssueListViewModel alloc]initWithJsonDictionary:model];
+    model.isRead = YES;
+    IssueDetailsVC *detailsVC = [[IssueDetailsVC alloc]init];
+    detailsVC.model = viewModel;
+    [self.navigationController pushViewController:detailsVC animated:YES];
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
